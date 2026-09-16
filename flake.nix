aba23804c56a72a79446e28d72aa3ae80e7ea9d4
Devin Droddy <devin@justdeeevin.dev>
2026-09-16 16:24:44 -0400

    init

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShell.${system} = pkgs.mkShell {
+        packages = with pkgs; [ nodejs ];
+      };
+    };
+}
